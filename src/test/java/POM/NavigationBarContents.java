8ef4ec419aa4a44c5b5a0adb6b1e97ce0fc2bb38
Abdullah <abdllahbrdk@gmail.com>
2022-09-17 12:09:02 -0400

    Updates

--- src/test/java/POM/NavigationBarContents.java
+++ src/test/java/POM/NavigationBarContents.java
@@ -1,5 +1,22 @@
 package POM;
 
-public class NavigationBarContents {
+import Utils.BaseDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class NavigationBarContents extends BasePOM {
+
+    public NavigationBarContents() {
+        PageFactory.initElements(BaseDriver.getDriver(), this);
+    }
+
+    @FindBy(xpath="(//span[text()='Setup'])[1]")
+    private WebElement setupButton;
+
+    @FindBy(xpath = "//span[text()='Parameters']")
+    private WebElement parametersButton;
+
+
 
 }
